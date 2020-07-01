215f6aa0f4a3931cd0a973f1903f499bdd64e21b
PravinSelva5 <selvarap@mcmaster.ca>
2020-06-30 23:26:00 -0400

    Added python scripts from tutorial

---
+++ Python/travel_spots.py
@@ -0,0 +1,8 @@
+import random
+
+countries = ["Portugal", "Argentina", "South Africa", "Germany", "Spain"]
+
+random_countries = random.choice(countries)
+
+
+print(f"The country I will be visiting is {random_countries}!")

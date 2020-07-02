54f4e130ae8ebc82297aa169187ecf9656cbfbfb
PravinSelva5 <selvarap@mcmaster.ca>
2020-07-02 15:41:03 -0400

    Updated scripts added to Python & HTML folders

---
+++ Python/if.py
@@ -0,0 +1,39 @@
+answer = input("Do you want to hear a joke? ")
+
+if answer == "Yes":
+    print("I'm against picketing, but I don't know how to show it")
+elif answer == "No":
+    print("Fine.")
+else:
+    print("I don't understand.")
+
+
+''' 
+SOME NOTES
+----------------------------------------------------------------
+Tabs and whitespace is REALLY important in Python
+Remember you can't tab and preform 4 spaces in the same file in python. You'll get an error.
+
+You'll get TabError: inconsistent use of tabs and spaces in indentation. 
+
+You can avoid this error by overriding your text editor settings to change tabs to spaces.
+
+
+
+TRUTH TERMS
+----------------------------------------------------------------
+==    [EQUAL TO]
+!=    [NOT EQUAL TO]
+>     [GREATER THAN]
+<     [LESS THAN]
+not 
+and 
+or
+in   
+
+Example for in:
+
+people = ["Mattan", "Chris", "Sarah"]
+"Mattan" in people  # will return True
+'''
+

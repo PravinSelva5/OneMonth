5f0c715217fa08e981c5a3179e706375522309e6
PravinSelva5 <selvarap@mcmaster.ca>
2020-07-04 23:23:37 -0400

    function's challenge added

--- Python/functions.py
+++ Python/functions.py
@@ -18,3 +18,14 @@ print(age_in_dog_years(4))
 #-------------------------------------------#
 # A function can return only one thing!
 #------------------------------------------#
+
+
+#-------------------------------------------#
+#  FUNCTIONS CHALLENGE
+#------------------------------------------#
+
+def uppercase_and_reverse(text):
+    
+    return text.upper()[::-1].strip('.')
+
+print(uppercase_and_reverse("Do not go gentle into that good night."))

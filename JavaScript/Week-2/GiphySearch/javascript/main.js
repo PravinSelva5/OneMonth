de3a41a6873a8c1040aa4f394f5019cf8ce8e26f
PravinSelva5 <selvarap@mcmaster.ca>
2020-08-06 22:09:56 -0400

    worked on grabbing information from Giphy API

--- JavaScript/Week-2/GiphySearch/javascript/main.js
+++ JavaScript/Week-2/GiphySearch/javascript/main.js
@@ -8,12 +8,30 @@ document.querySelector(".js-go").addEventListener('click', function(){
 });
 
 
-
+document.querySelector(".js-userinput").addEventListener('keyup', function(){
+    var input = document.querySelector("input").value;
+    console.log(input);
+    
+    if (event.which === 13) {
+        pushToDOM(input);
+    };
+});
 
 
 /* 2. Do the data stuff with the API */
 
+var url = "http://api.giphy.com/v1/gifs/search?q=funny+cat&api_key=dc6zaTOxFJmzC";
 
+// AJAX Request
+var GiphyAJAXCall = new XMLHttpRequest();
+GiphyAJAXCall.open('GET', url);
+GiphyAJAXCall.send();
+
+GiphyAJAXCall.addEventListener('load', function(event) {
+
+    var data = event.target.response;
+    console.log(data);
+});
 
 
 

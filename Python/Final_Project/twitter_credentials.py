46d27a3aa3164ff7becca71a7adc9471060c460a
PravinSelva5 <selvarap@mcmaster.ca>
2020-09-20 19:54:27 -0400

    played around with twitter's tweepy API to stream tweets based on a list of filters

---
+++ Python/Final_Project/twitter_credentials.py
@@ -0,0 +1,5 @@
+# Variables that contain the user credentials to access Twitter APIdatetime A combination of a date and a time. Attributes: ()
+ACCESS_TOKEN = ""
+ACCESS_TOKEN_SECRET = ""
+CONSUMER_KEY = ""
+CONSUMER_SECRET = ""

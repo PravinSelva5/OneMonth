d2ba0638e8bafc3353ef46f765dcd60d29f4a2cb
PravinSelva5 <selvarap@mcmaster.ca>
2020-09-24 16:39:11 -0400

    worked on pulling information from SoundCloud's API and showing separate cards of songs based off the user's search

--- JavaScript/Week-3/SoundCloudPlayer/javascript/main.js
+++ JavaScript/Week-3/SoundCloudPlayer/javascript/main.js
@@ -6,16 +6,84 @@
 
 /* 2. Query Soundcloud API */
 
-SC.initialize({
-  client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
+var soundCloudAPI = {};
+
+soundCloudAPI.init = function() {
+  SC.initialize({
+    client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
+  });
+}
+
+soundCloudAPI.init();
+
+soundCloudAPI.getTrack = function(inputValue) {
+  // find all sounds of buskers licensed under 'creative commons share alike'
+  SC.get('/tracks', {
+    q: inputValue
+  }).then(function(tracks) { 
+    console.log(tracks);
+    soundCloudAPI.renderTracks(tracks);
+  });
+}
+
+soundCloudAPI.getTrack("tory lanez");
+
+
+soundCloudAPI.renderTracks = function(tracks) {
+
+    tracks.forEach(function(track) {
+        
+      //Creating a div for a new card and appending it into html
+      var card = document.createElement('div');
+      card.classList.add('card');
+
+      // image
+      var cardImage = document.createElement('div');
+      cardImage.classList.add('image');
+
+      var image_img = document.createElement('img');
+      image_img.classList.add('image_img');
+      image_img.src = track.artwork_url || 'http://lorempixel.com/400/200/';
+
+      cardImage.appendChild(image_img);
+
+      // content 
+      var content = document.createElement('div');
+      content.classList.add('content');
+
+      var header = document.createElement('div');
+      header.classList.add('header');
+      header.innerHTML = '<a href=" '+ track.permalink_url +' target="_blank">'+ track.title +'</a>';
+
+      // button
+      var button = document.createElement('div');
+      button.classList.add('ui', 'bottom', 'attached', 'button', 'js-button');
+
+      var icon = document.createElement('i');
+      icon.classList.add('add', 'icon');
+
+      var buttonText = document.createElement('span');
+      buttonText.innerHTML = 'Add to playlist';
+
+
+      // APPEND CHILD
+      content.appendChild(header);
+
+      button.appendChild(icon);
+      button.appendChild(buttonText);
+
+      card.appendChild(cardImage);
+      card.appendChild(content);
+      card.appendChild(button);
+
+      var searchResults = document.querySelector('.js-search-results');
+      searchResults.appendChild(card);
 });
 
-// find all sounds of buskers licensed under 'creative commons share alike'
-SC.get('/tracks', {
-  q: 'the weeknd'
-}).then(function(tracks) {
-  console.log(tracks);
-});
+}
+
+
+
 
 
 /* 3. Display the cards */

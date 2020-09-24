99ec97a092aae126648d96847a491eafd34a70cd
PravinSelva5 <selvarap@mcmaster.ca>
2020-09-23 21:42:54 -0400

    working on a soundcloud app with the help of its API

---
+++ JavaScript/Week-3/SoundCloudPlayer/javascript/main.js
@@ -0,0 +1,27 @@
+/* 1. Search */
+
+
+
+
+
+/* 2. Query Soundcloud API */
+
+SC.initialize({
+  client_id: 'cd9be64eeb32d1741c17cb39e41d254d'
+});
+
+// find all sounds of buskers licensed under 'creative commons share alike'
+SC.get('/tracks', {
+  q: 'the weeknd'
+}).then(function(tracks) {
+  console.log(tracks);
+});
+
+
+/* 3. Display the cards */
+
+
+
+
+
+/* 4. Add to playlist and play */
